7558d404fc4bb2c51604c49eb17cea2b9ae97c74
Anson Ho <48861078+spectroscopycafe@users.noreply.github.com>
2021-03-10 12:52:02 +0000

    initial commit

---
+++ backend/experiment.py
@@ -0,0 +1,84 @@
+#Sean Billings, 2015
+# Minor edits by Anson Ho, 2021
+
+import subprocess
+import os
+from backend import mpbParser
+
+# An experiment is a class representation for an mpb command line run
+# by default the mpb run is split across 6 cores, but this can be augments
+
+# should be refactored as W1Experiment
+# and no parameter adjustments
+class W1Experiment(object):
+
+        # initializes the W1Experiment with default parameters (acts as a constructor specification)
+        def __init__(self, mpb, inputFile, outputFile, parserIn=mpbParser, parseStrat="MAXGBP"):
+            """
+
+            :param mpb: command line path for the mpb program
+            :param inputFile: specifies the structure of the waveguide
+            :param outputFile: records output such as tefreqs, loss
+            """
+            self.inputFile = inputFile
+            self.mpb = mpb
+            self.outputFile = outputFile
+            self.paramVectorString = ""
+            self.calculationType = " calculation-type=4"
+            self.c1 = 4 # constant for out of plane loss
+            self.c2 = 220 # constant for backscattering loss
+            self.fieldFraction= 0.827
+            self.band = 23
+            self.ks = 0.3
+            self.ke = 0.5
+            self.kinterp = 39
+            self.split = "-split 6"
+            self.dim3 = False
+            self.parser=parserIn
+            self.parseStrategy=parseStrat
+
+        # execute the current instantiation of W1Experiment as a mpb calculation via the command line
+        def perform(self):
+
+            FNULL = open(os.devnull, 'w')
+            subprocess.call(self.mpb + self.split + " Ks="+ str(self.ks) + " Ke=" + str(self.ke) + " Kinterp=" + str(self.kinterp) + self.calculationType + self.paramVectorString + ' %s > %s' %(self.inputFile, self.outputFile),
+                            shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+
+        # set the parameters (as coded in the ctl with the format define-param) to values according to paramMap
+        # paramMap example
+        # {'p2': 0.07646386156785503, 'p3': 0.043493821552317305, 'p1': 0.07235772695508993,
+        # 'r1': 0.017137050399521098, 'r2': 0.2053715543744835, 'r3': 0.03296599949382803,
+        # 's3': 0.011459725712917944, 's2': 0.09901968783066335, 's1': 0.17175319110067366}
+        def setParams(self, paramMap):
+                self.paramVectorString = ""
+                for x in paramMap.keys():
+                        if 'r' in x or 'a' in x: 
+                                self.paramVectorString += " " + x + "=" + str(paramMap[x])
+                        else:
+                                self.paramVectorString += " " + x + "=" + str(paramMap[x])
+                        
+                                
+
+        # adjusts the calculation type (according to the type specifications as in W1_2D_v03.ctl)
+        def setCalculationType(self, calcType):
+            self.calculationType = " calculation-type=" + str(calcType)
+
+        # a utility function to quickly calculate the Group Index, average Loss, BGP, etc
+        # from the experiment instantiation
+        # see mpbParser.py for output list format
+        def extractFunctionParams(self):
+            self.perform()
+            if self.dim3:
+                return self.parser.parseObjFunctionParams3D(self, self.parseStrategy)
+
+            return self.parser.parseObjFunctionParams(self, self.parseStrategy)
+
+        # determine which band we are considering for a given photonic crystal
+        def setBand(self, newBand):
+            self.band = newBand
+
+        def setSplit(self, split_num):
+            self.split = "-split " + str(split_num)
+
+        def noSplit(self):
+            self.split = ""
---
+++ train-set-generator.py
@@ -0,0 +1,74 @@
+# Anson Ho, 2021
+# Python script for generating training sets for the neural network
+
+# Imports
+from backend.experiment import W1Experiment
+from backend import constraints
+from backend import objectiveFunctions
+from random import uniform
+import pandas as pd
+
+def main():
+    # paths to files
+    mpb = "/usr/bin/mpb"
+    inputFile = "/home/nanophotgrp/Desktop/PCO/WaveguideCTL/W1_2D_v04.ctl.txt"
+    outputLog = "/home/nanophotgrp/Desktop/PCO/test-run.out"
+    outputCSV = "/home/nanophotgrp/Desktop/PCO/2021-03-10_train-set.csv"
+
+    # initialise parameter map
+    # pars = {'r0': 0.1, 'r1': 0.2, 'r2': 0.2845, 'r3': 0.290696, 's3': -0.004733, 's2': -0.000407, 's1': -0.048862, 'p1':0.3, 'p2':0.3, 'p3':0.3}
+    initPars = {'r0': 0, 'r1': 0, 'r2': 0, 'r3': 0, 's1': 0, 's2': 0, 's3': 0, 'p1': 0, 'p2': 0, 'p3': 0}
+    constraintFunctions = [constraints.latticeConstraintsLD]
+
+    # number of runs
+    runs = 1
+    outputData = []
+
+    # create datasets
+    for i in range(runs):
+        print("STARTING RUN {} OUT OF {}".format(i+1, runs))
+        
+        # randomise parameters with constraints
+        # make copy of initPars
+        randomPars = {param: value for param,value in initPars.items()}
+        randomPars['r0'] = uniform(0.2, 0.4)
+        randomPars['r1'] = uniform(0.2, 0.4)
+        randomPars['r2'] = uniform(0.2, 0.4)
+        randomPars['r3'] = uniform(0.2, 0.4)
+        randomPars['s1'] = uniform(-0.4, 0.4)
+        randomPars['s2'] = uniform(-0.4, 0.4)
+        randomPars['s3'] = uniform(-0.4, 0.4)
+        randomPars['p1'] = uniform(-0.4, 0.4)
+        randomPars['p2'] = uniform(-0.4, 0.4)
+        randomPars['p3'] = uniform(-0.4, 0.4)
+        #print("BEFORE", randomPars) # debug
+        
+        constrainPars = constraints.fix(randomPars, constraintFunctions)
+        #print("AFTER", constrainPars) # debug
+        
+        # set up experiment
+        experiment = W1Experiment(mpb, inputFile, outputLog)
+        experiment.setParams(constrainPars)
+        experiment.setCalculationType(4) # fails with other calculation types
+        experiment.setBand(23)
+        experiment.kinterp = 19
+        experiment.dim3 = False # 2D
+        experiment.split = "-split 15"
+
+        # run experiment and extract FOMs
+        getFOMs = experiment.extractFunctionParams()
+        #print(foms) # debug
+        
+        # combine dictionaries
+        outputDict = {**constrainPars, **getFOMs}
+        outputData.append(outputDict)
+        print(outputDict)
+        
+        print("END OF RUN NO. {} \n".format(i+1))
+
+    # print(outputData) # debug
+    df = pd.DataFrame(outputData)
+    df.to_csv(outputCSV)
+
+if __name__ == "__main__":
+    main()
